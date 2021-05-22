2da8f3850db1ea2c88d56d24f261c24bbb23838c
Sergey <sergios33033@gmail.com>
2021-05-22 22:24:33 +0300

    F

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
 web: webpack --mode production --config webpack.server.config.js && webpack --mode production --config webpack.prod.config.js && node ./dist/server.js
+
+heroku ps:scale web=0
+heroku ps:scale worker=1
+worker: node src/index.js
--- src/server/server.prod.js
+++ src/server/server.prod.js
@@ -27,6 +27,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, '0.0.0.0', () => {
   console.log(`Server is listening on port ${(process.env.PORT || 3000)}`);
 });
