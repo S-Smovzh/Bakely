929bc2b632165b9cefbe78838b84a5634fe70970
Sergey <sergios33033@gmail.com>
2021-05-22 17:59:08 +0300

    Fixed babel.config.

--- babel.config.js
+++ babel.config.js
@@ -16,8 +16,8 @@ module.exports = (api) => {
       '@babel/plugin-transform-classes'
     ],
     presets: [
-      '@babel/preset-env', { 'targets': { 'node': 'current' } },
-      '@babel/preset-react', { 'targets': { 'node': 'current' } }
+      '@babel/preset-env',
+      '@babel/preset-react'
     ],
     env: {
       // 'development': {
